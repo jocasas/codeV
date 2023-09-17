d721647eb317694f549d079eaadef3a5ec8f1e14
jocasas <102597441+jocasas@users.noreply.github.com>
2023-09-16 21:56:54 -0300

    Add Compile function

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const { exec } = require("child_process"); //funcion de node para ejecutar comandos en shell de la maquina
+const app = express();
 
-app.get("/api",(req,res)=>{
-    res.json({"users":["hola","desde","express"]})
-})
+app.get("/api", (req, res) => {
+  const string = ["print('hola mundo')"];
+  const pythonCode = string;
+  exec(`python -c"${pythonCode}"`, (error, stdout, stderr) => {
+    if (error) {
+      // Handle Python execution errors
+      res.status(500).json({ error: error.message });
+    } else {
+      // Send the Python execution result back as a response
+      console.log(stdout);
+      res.json({ result: stdout });
+    }
+  });
+});
 
-app.listen(5000,()=>{console.log("Servidor abierto en puerto 5000"),console.log("At site http://localhost:5000/api")})
+app.listen(5000, () => {
+  console.log("Servidor abierto en puerto 5000"),
+    console.log("At site http://localhost:5000/api");
+});
