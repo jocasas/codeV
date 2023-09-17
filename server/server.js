2d6cb0ff809ef089bf2cebd94408cee32461909e
jocasas <102597441+jocasas@users.noreply.github.com>
2023-09-17 17:05:36 -0300

    Redone

--- server/server.js
+++ server/server.js
@@ -1,23 +1,41 @@
 const express = require("express");
 const { exec } = require("child_process"); //funcion de node para ejecutar comandos en shell de la maquina
+const fs = require("fs");
 const app = express();
+const port = 5000;
 
 app.get("/api", (req, res) => {
-  const string = ["print('hola mundo')"];
-  const pythonCode = string;
-  exec(`python -c"${pythonCode}"`, (error, stdout, stderr) => {
-    if (error) {
-      // Handle Python execution errors
-      res.status(500).json({ error: error.message });
-    } else {
-      // Send the Python execution result back as a response
-      console.log(stdout);
-      res.json({ result: stdout });
-    }
-  });
+  try {
+    const pythonCode = 'print("hola\\n4")'; // Example code with double quotes
+
+    /**
+     * Escribe el codigo a un archivo temporal, esto lo hice para
+       saltarme un problema, quizas lo quite al final si lo soluciono
+     */
+    fs.writeFileSync("temp.py", pythonCode);
+
+    exec(`python temp.py`, (error, stdout, stderr) => {
+      fs.unlinkSync("temp.py");
+
+      if (error) {
+        res.status(500).json({ error: error.message });
+      } else {
+        /**
+         * La siguiente funcion es experimental y estoy creando una funcion
+           para ver si la integro, Limpia el output del \r \n que hace la 
+           interpretacion por default cuando interpreta scripts
+         * const CleanOutput = stdout.replace(/[\r\n]+/g, "");
+        */
+        console.log(stdout);
+        res.json({ result: stdout });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-app.listen(5000, () => {
-  console.log("Servidor abierto en puerto 5000"),
-    console.log("At site http://localhost:5000/api");
+app.listen(port, () => {
+  console.log(`Servidor abierto en puerto ${port}`),
+    console.log(`En el sitio http://localhost:${port}/api`);
 });
