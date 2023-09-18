9f93efc2a6555b232850380273ee4d371a0cd12d
jocasas <102597441+jocasas@users.noreply.github.com>
2023-09-18 00:42:29 -0300

    Transform 2 OOP

--- server/server.js
+++ server/server.js
@@ -1,41 +1,41 @@
 const express = require("express");
-const { exec } = require("child_process"); //funcion de node para ejecutar comandos en shell de la maquina
+const { exec } = require("child_process");
 const fs = require("fs");
-const app = express();
-const port = 5000;
 
-app.get("/api", (req, res) => {
-  try {
-    const pythonCode = 'print("hola\\n4")'; // Example code with double quotes
+class PythonRunner {
+  constructor() {
+    this.app = express();
+    this.port = 5000;
 
-    /**
-     * Escribe el codigo a un archivo temporal, esto lo hice para
-       saltarme un problema, quizas lo quite al final si lo soluciono
-     */
-    fs.writeFileSync("temp.py", pythonCode);
+    this.app.get("/api", this.handleApiRequest.bind(this));
 
-    exec(`python temp.py`, (error, stdout, stderr) => {
-      fs.unlinkSync("temp.py");
-
-      if (error) {
-        res.status(500).json({ error: error.message });
-      } else {
-        /**
-         * La siguiente funcion es experimental y estoy creando una funcion
-           para ver si la integro, Limpia el output del \r \n que hace la 
-           interpretacion por default cuando interpreta scripts
-         * const CleanOutput = stdout.replace(/[\r\n]+/g, "");
-        */
-        console.log(stdout);
-        res.json({ result: stdout });
-      }
+    this.app.listen(this.port, () => {
+      console.log(`Server is listening on port ${this.port}`);
+      console.log(`Visit http://localhost:${this.port}/api`);
     });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
   }
-});
 
-app.listen(port, () => {
-  console.log(`Servidor abierto en puerto ${port}`),
-    console.log(`En el sitio http://localhost:${port}/api`);
-});
+  handleApiRequest(req, res) {
+    try {
+      const pythonCode = 'print("hola\\n4")';
+
+      // Write the Python code to a temporary file
+      fs.writeFileSync("temp.py", pythonCode);
+
+      exec(`python temp.py`, (error, stdout, stderr) => {
+        fs.unlinkSync("temp.py");
+
+        if (error) {
+          res.status(500).json({ error: error.message });
+        } else {
+          console.log(stdout);
+          res.json({ result: stdout });
+        }
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+}
+
+new PythonRunner();
