d557ef4924f64a1950553dd44212316638acaf50
jocasas <102597441+jocasas@users.noreply.github.com>
2023-11-11 14:46:51 -0300

    wip

--- clientwo/src/app/(routes)/testing/page.tsx
+++ clientwo/src/app/(routes)/testing/page.tsx
@@ -1,32 +1,32 @@
 "use client";
 import axios from "axios";
+import { useState } from "react";
 // `app/page.tsx` is the UI for the `/` URL
 export default function Page() {
+  const [exercises, setExercises] = useState();
   const showExercise = async () => {
-    console.log("Iniciando funcion de ejercicios");
+    console.log("Iniciando FETCH");
     try {
-      console.log("Intentando hacer fetch de los ejercicios");
-      const response = await axios.get(
-        "http://localhost:5000/apiGetExercise",
-        {}
-      );
-      console.log(JSON.stringify(response.data));
-      if (response.status === 200) {
-        console.log("Se recibieron los ejercicios");
-      } else {
-        console.error("Error al recibir ejercicio");
-      }
+      const response = await axios.get("http://localhost:5000/apiGetExercise", {
+        params: {
+          language: "python",
+          difficult: 1,
+        },
+      });
+      setExercises(response.data);
+      console.log("Se hizo fetch correctamente");
     } catch (error) {
       console.error(error);
+      console.log("Hubo un problema en el fetch");
     }
   };
 
   return (
     <div>
-      <button onClick={showExercise} className="bg-white p-5">
+      <button onClick={showExercise} className="bg-white m-40 p-5">
         presioname
       </button>
-      <h1>Hello, Home page!</h1>
+      <h1 className="text-gray-100">{exercises}</h1>
     </div>
   );
 }
