8124778d8a8ce2def956d5b7c144f8c1722570e8
jocasas <102597441+jocasas@users.noreply.github.com>
2023-11-12 20:56:33 -0400

    terminal idea

--- clientwo/src/Components/AceEditor/AceEditor.tsx
+++ clientwo/src/Components/AceEditor/AceEditor.tsx
@@ -11,20 +11,35 @@ function App() {
   };
 
   return (
-    <div className="p-10 flex">
-      <div className="w-1/2">
-        <AceEditor
-          mode="python"
-          theme="terminal"
-          onChange={handleCodeChange}
-          value={codeString}
-          name="code-editor"
-          editorProps={{ $blockScrolling: true }}
-        />
+    <div>
+      <div className="pr-10 pl-10 flex flex-col sm:flex-row">
+        <div className="flex-1 p-2 rounded overflow-auto bg-black text-white border-8 border-black">
+          {/* Another div parallel to the editor */}
+          <h2 className="text-center">Problema</h2>
+          <p>Content of the new pane...</p>
+        </div>
+        <div className="border-8 border-black rounded flex-1 ml-2 mb-2 sm:mb-0">
+          <AceEditor
+            mode="python"
+            theme="terminal"
+            onChange={handleCodeChange}
+            value={codeString}
+            name="code-editor"
+            editorProps={{ $blockScrolling: true }}
+            style={{ width: "100%" }}
+            showPrintMargin={false}
+            fontSize={16}
+          />
+        </div>
       </div>
-      <div className="w-1/2 p-2 border rounded overflow-auto">
-        {/* Another div parallel to the editor */}
-        <h1 className="text-gray-50 text-center">holaaa</h1>
+      {/* New pane below the parallel panes */}
+      <div className="pr-10 pl-10 flex flex-col sm:flex-row">
+        <div className="flex-1 mt-2 p-2 rounded overflow-auto bg-black text-white border-8 border-black">
+          {/* Another div parallel to the editor */}
+          <h1 className="text-gray-50 text-center">Output</h1>
+          <p className="text-green-400">$ Your terminal-like text here...</p>
+          <p className="text-gray-300">Output: Something cool!</p>
+        </div>
       </div>
     </div>
   );
